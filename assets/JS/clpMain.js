1a81598f1be14f145232856079988b5f5f090f71
Jennifer Plaza <jpplaza@student.fullsail.edu>
2024-11-04 19:49:44 -0500

    added images,logos, css, js, and index html files

---
+++ assets/JS/clpMain.js
@@ -0,0 +1,12 @@
+alert('js file');
+//Navigation Menu onclick functionality for mobile sizes
+//function appear() {
+  //alert('appear');
+  //var navigationList = document.getElementById('navList');
+
+  //if (navigationList.style.display == 'none') {
+    navigationList.style.display = 'block';
+  //} else {
+    navigationList.style.display == 'none';
+  //}
+//}
