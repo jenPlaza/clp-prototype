d04670aec2fb28d4a45934b04964c00a4db2eb21
Jennifer Plaza <jpplaza@student.fullsail.edu>
2024-11-07 23:47:57 -0500

    Added style and functionality to index page from 375 to 768px

--- assets/JS/clpMain.js
+++ assets/JS/clpMain.js
@@ -1,12 +1,149 @@
-alert('js file');
-//Navigation Menu onclick functionality for mobile sizes
-//function appear() {
-  //alert('appear');
-  //var navigationList = document.getElementById('navList');
+console.log('js main');
+//NAV Scrolling
+const navMenu = document.querySelector('.scroll-in-place');
+console.log(window.scrollY);
+(window.onscroll = function () {
+  if (window.scrollY > 100) {
+    navMenu.classList.add('nav-scroll');
+  } else {
+    navMenu.classList.remove('nav-scroll');
+  }
+})();
 
-  //if (navigationList.style.display == 'none') {
-    navigationList.style.display = 'block';
-  //} else {
-    navigationList.style.display == 'none';
-  //}
-//}
+//Navigation Icon Menu onclick functionality for mobile sizes
+function appear() {
+  var headerBar = document.querySelector('.scroll-in-place');
+  var navigationList = document.getElementById('navList');
+  if (navigationList.style.display == 'none') {
+    headerBar.style.background = 'var(--red)';
+    navigationList.style.display = 'contents';
+  } else if (navigationList.style.display == 'contents') {
+    headerBar.style.background = 'revert-layer';
+    navigationList.style.display = 'none';
+  }
+}
+//Navbar avtivities dropdown content
+function displayContent() {
+  var navBar = document.getElementById('navList');
+  var navList = navBar.querySelector('.list');
+  var dcontent = document.querySelector('.dropdown-content');
+  if (dcontent.style.display == 'none') {
+    dcontent.style.display = 'flex';
+    navList.classList.add('top255');
+  } else if (dcontent.style.display == 'flex') {
+    dcontent.style.display = 'none';
+    navList.classList.remove('top255');
+  }
+}
+function displayActivities() {
+  var navBar = document.getElementById('navList');
+  var navList = navBar.querySelector('.list');
+  var dcontent = document.querySelector('.dropdown-content');
+  if (dcontent.style.display == 'none') {
+    dcontent.style.display = 'flex';
+    navList.classList.add('top255');
+  } else if (dcontent.style.display == 'flex') {
+    dcontent.style.display = 'none';
+    navList.classList.remove('top255');
+  }
+}
+//Navigation Menu extension onclick functionality for mobile sizes
+function extendMenu() {
+  var navList = document.querySelector('.list');
+  const navshow = document.getElementsByClassName('nav-link');
+  const openLGMenu = document.querySelector('.nav-open');
+  const closeLGMenu = document.querySelector('.nav-close');
+  var dcontent = document.querySelector('.dropdown-content');
+  //testing
+  //console.log('navshow: ', navshow.length);
+  navList.classList.add('max');
+  openLGMenu.style.display = 'none';
+  closeLGMenu.style.display = 'block';
+  dcontent.classList.add('modifyMargin');
+  for (var i = 0; i < navshow.length; i++) {
+    //console.log(navshow[i]);
+    navshow[i].classList.add('show');
+  }
+}
+//Navigation Menu extension onclick functionality for mobile sizes
+function shortenMenu() {
+  var navList = document.querySelector('.list');
+  const navshow = document.getElementsByClassName('nav-link');
+  const closeLGMenu = document.querySelector('.nav-close');
+  const openLGMenu = document.querySelector('.nav-open');
+  var dcontent = document.querySelector('.dropdown-content');
+  //console.log('navshow: ', navshow.length);
+  navList.classList.remove('max');
+  navList.classList.remove('top255');
+  closeLGMenu.style.display = 'none';
+  openLGMenu.style.display = 'block';
+  dcontent.classList.remove('modifyMargin');
+  dcontent.style.display = 'none';
+  for (var i = 0; i < navshow.length; i++) {
+    //console.log(navshow[i]);
+    navshow[i].classList.remove('show');
+  }
+}
+
+const mediaQueryTablet = window.matchMedia('(min-width: 768px)');
+const mediaQueryMobile = window.matchMedia('(min-width: 375px)');
+if (mediaQueryTablet.matches) {
+  const navicon = document.getElementsByClassName('nav-icon');
+  const navshow = document.getElementsByClassName('nav-link');
+
+  for (var i = 0; i < navicon.length; i++) {
+    navicon[i].style.display = 'none';
+  }
+  for (var p = 0; p < navshow.length; p++) {
+    navshow[p].style.display = 'block';
+  }
+}
+
+//FOOTER LOCATION
+var locate = '<figure id="logo">';
+locate +=
+  '<img src="' +
+  company[0].logo_info[0].img_src +
+  '" alt="logo" width="40px" height="40px" /></figure>';
+locate += '<div><p><b>' + company[0].company_name + '</b></p>';
+locate += '<address>' + company[0].company_add + '</address>';
+locate += '<div><p><b>' + company[0].company_phone + '</b></p></div></div>';
+
+document.getElementById('location').innerHTML = locate;
+
+//FOOTER SOCIAL
+
+var clp = '<ul>';
+if (mediaQueryMobile.matches) {
+  for (var k = 0; k < socialArray.length; k++) {
+    clp += '<li>';
+    clp +=
+      '<a href="' + socialArray[k].media_link[1] + '" target="_blank"><figure>';
+    clp +=
+      '<img src="' +
+      socialArray[k].media_src[0] +
+      '" alt="' +
+      socialArray[k].media_alt +
+      '" class="social-icon"/>';
+    clp += '</figure></a></li>';
+  }
+  clp += '</ul>';
+  document.getElementById('socialM').innerHTML = clp;
+}
+if (mediaQueryTablet.matches) {
+  var clp = '<ul>';
+  for (var k = 0; k < socialArray.length; k++) {
+    clp += '<li>';
+    clp +=
+      '<a href="' + socialArray[k].media_link[1] + '" target="_blank"><figure>';
+    clp +=
+      '<img src="' +
+      socialArray[k].media_src[1] +
+      '" alt="' +
+      socialArray[k].media_alt +
+      '" class="social-icon"/>';
+    clp += '</figure></a></li>';
+  }
+  clp += '</ul>';
+  document.getElementById('socialM').innerHTML = clp;
+}
---
+++ assets/js/clpData.js
@@ -0,0 +1,93 @@
+console.log('js data');
+//NAVIGATION MENU
+var navigationArray = [
+  {
+    link_id: 'home',
+    link_name: 'Home',
+    link_href: './index.html',
+  },
+  {
+    link_id: 'about',
+    link_name: 'About Us',
+    link_href: './about-us.html',
+  },
+  {
+    link_id: 'events',
+    link_name: 'Events',
+    link_href: './event.html',
+  },
+  {
+    link_id: 'competencias',
+    link_name: 'Tournaments',
+    link_href: './tournaments.html',
+  },
+  {
+    link_id: 'contact',
+    link_name: 'Contact',
+    link_href: './index#contact.html',
+  },
+];
+
+var company = [
+  {
+    company_name: 'Club Los Paltos',
+    company_add: '239 Meadow View Dr. Powell, Ohio(OH), 43065',
+    company_phone: '(740) 548-2041',
+    logo_info: [
+      {
+        link_href: '.index.html',
+        img_src: './assets/images/logos/clublospaltos.png',
+        img_alt: 'Club Los Paltos logo',
+        img_caption: 'Club Los Paltos',
+      },
+    ],
+  },
+];
+
+var socialArray = [
+  {
+    media_id: 'email',
+    media_link: '/',
+    media_src: [
+      'assets/images/icons/mail-white.png',
+      'assets/images/icons/message-bk.png',
+    ],
+    media_alt: 'email icon',
+  },
+  {
+    media_id: 'phone',
+    media_link: 'tel:1-232-75-769929',
+    media_src: [
+      'assets/images/icons/phone-white.png',
+      'assets/images/icons/phone-bk.png',
+    ],
+    media_alt: 'phone icon',
+  },
+  {
+    media_id: 'linkedIn',
+    media_link: '/',
+    media_src: [
+      'assets/images/icons/linkedin-white.png',
+      'assets/images/icons/linkedin-bk.png',
+    ],
+    media_alt: 'linkedIn icon',
+  },
+  {
+    media_id: 'facebook',
+    media_link: '/',
+    media_src: [
+      'assets/images/icons/facebook-white.png',
+      'assets/images/icons/facebook-bk.png',
+    ],
+    media_alt: 'facebook icon',
+  },
+  {
+    media_id: 'instgram',
+    media_link: '/',
+    media_src: [
+      'assets/images/icons/instagram-white.png',
+      'assets/images/icons/instagram-bk.png',
+    ],
+    media_alt: 'instgram icon',
+  },
+];
